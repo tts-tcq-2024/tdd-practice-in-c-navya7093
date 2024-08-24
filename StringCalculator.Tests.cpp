1a5b3ae714e62f45e5c0ea394b79985f89bcfa27
navya7093 <navyakontala@gmail.com>
2024-08-24 18:56:15 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -2,65 +2,8 @@
 #include "StringCalculator.h"
 
 TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
-    int expectedresult = 0;
-    const char* input = "Hello, world!";
+    int expectedResult = 0;
+    const char* input = "";
     int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
-    int expectedresult = 0;
-    const char* input = "0";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
-    int expectedresult = 3;
-    const char*  input = "1,2";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
-    int expectedresult = 6;
-    const char*  input = "1\n2,3";
-    int result =add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
-    int expectedresult = 1;
-    const char*  input = "1,1001";
-    int result =add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
-    int expectedresult = 3;
-    const char*  input = "//;\n1;2";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiterAtStart) {
-    const char*  input = "/3,4";
-    add(input);
-}
- TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiterAtStart1) {
-    const char*  input = "/2";
-    add(input);
-}
- 
-
-TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
-    const char* input = "-1,3,-5";
-     add(input);
-}
- 
-
-TEST(StringCalculatorAddTests, ExpectSumWithEmpptyString) {
-    int expectedresult = 0;
-    const char*  input = "";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
+    ASSERT_EQ(result, expectedResult);
 }
