e4fc0e2c9ebe5bcc93850bfd1721ba6541f9087c
navya7093 <navyakontala@gmail.com>
2024-08-24 19:45:04 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -19,23 +19,3 @@ TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
-    int expectedresult = 6;
-    const char*  input = "1\n2,3";
-    int result =add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-// Test case to cover the missing branch where both conditions are true
-TEST(StringCalculatorAddTests, ExpectSumForNoDelimiter) {
-    int expectedresult = 7;
-    const char* input = "7";  // A string without comma or newline
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-TEST(StringCalculatorAddTests, ExpectSumForBothCommaAndNewline) {
-    int expectedresult = 6;
-    const char* input = "1,2\n3";  // String with both a comma and newline
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
