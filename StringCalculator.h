84bb1cf4d3df76d94781b9b4567d6ae3f9cf8903
navya7093 <navyakontala@gmail.com>
2024-08-24 19:06:13 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,5 +1,7 @@
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
+#include <cstring>
+#include <cstdlib>
 
 int add(const char* numbers) {
     // Minimum code to pass the test
@@ -9,10 +11,6 @@ int add(const char* numbers) {
 #endif // STRINGCALCULATOR_H
 // StringCalculator.h
 
-#ifndef STRINGCALCULATOR_H
-#define STRINGCALCULATOR_H
-
-#include <cstring>
 
 int add(const char* numbers) {
     // Handle empty input
@@ -30,3 +28,36 @@ int add(const char* numbers) {
 }
 
 #endif // STRINGCALCULATOR_H
+// StringCalculator.h
+
+
+
+
+int add(const char* numbers) {
+    // Handle empty input
+    if (strcmp(numbers, "") == 0) {
+        return 0;
+    }
+    
+    // Handle single zero
+    if (strcmp(numbers, "0") == 0) {
+        return 0;
+    }
+    
+    // Handle two numbers separated by a comma
+    const char* delimiter = ",";
+    char* numbersCopy = strdup(numbers);
+    char* token = strtok(numbersCopy, delimiter);
+    int sum = 0;
+    
+    while (token != NULL) {
+        sum += atoi(token);
+        token = strtok(NULL, delimiter);
+    }
+
+    free(numbersCopy);
+    return sum;
+}
+
+#endif // STRINGCALCULATOR_H
+
